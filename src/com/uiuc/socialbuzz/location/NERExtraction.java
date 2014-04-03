f3c31b5153b62c7764c32b1ca6d3e16a1c9a3c6c
nehapawar <pawar2@illinois.edu>
2014-04-02 20:44:06 -0500

    SocialBuzz initial prototype

---
+++ src/com/uiuc/socialbuzz/location/NERExtraction.java
@@ -0,0 +1,8 @@
+package com.uiuc.socialbuzz.location;
+
+public class NERExtraction {
+	
+	//Mao's code here: write a method which takes in the tweet String 
+	//as input and returns an arraylist of entities extracted by NER as output
+
+}
---
+++ src/com/uiuc/socialbuzz/location/SocialBuzzLocation.java
@@ -0,0 +1,38 @@
+package com.uiuc.socialbuzz.location;
+
+public class SocialBuzzLocation {
+	String tweet = null;
+	SocialBuzzLocation(String tweet)
+	{
+		this.tweet = tweet;
+		
+		processTweet();
+	}
+	
+	
+	void processTweet()
+	{
+		//Create city and state hash maps
+		
+		//Create initial seed of locations
+		
+		
+		
+		//Call NER code, to get back an array of entities
+		
+		//Extract locations from 4sq URLs
+		
+		//get ngrams
+		
+		//get hashtags
+		
+		
+		
+		//Eliminate those which exist in cities and states
+		
+		//Check in pool
+		
+		//Check in OpenStreetMap
+	}
+
+}
