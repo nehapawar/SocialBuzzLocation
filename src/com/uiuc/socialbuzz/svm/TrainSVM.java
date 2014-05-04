ce7a4ed192f822d1d2d552f96c6826096fe1be57
nehapawar <pawar2@illinois.edu>
2014-05-04 19:22:53 -0400

    svm updates

--- src/com/uiuc/socialbuzz/svm/TrainSVM.java
+++ src/com/uiuc/socialbuzz/svm/TrainSVM.java
@@ -15,7 +15,9 @@ import java.sql.Statement;
 import java.text.ParseException;
 import java.util.Scanner;
 
+import jnisvmlight.KernelParam;
 import jnisvmlight.LabeledFeatureVector;
+import jnisvmlight.LearnParam;
 import jnisvmlight.SVMLightInterface;
 import jnisvmlight.SVMLightModel;
 import jnisvmlight.TrainingParameters;
@@ -26,7 +28,8 @@ public class TrainSVM
 {
 	
 	static int N = 18408;
-	static int M = 18;
+	static int M = 10;
+	//static int M = 18;
 	
 	public static void main(String[] args) throws SQLException, IOException {
 		// The trainer interface with the native communication to the SVM-light shared
@@ -81,7 +84,8 @@ public class TrainSVM
 				"4sq"
 			};*/
 		
-		int[] dimensions = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+		//int[] dimensions = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+		int[] dimensions = {1,2,3,4,5,6,7,8,9,10};
 		double[] values ; 
 		int label = 0; //fill up label
 		int i = 0;
@@ -92,7 +96,7 @@ public class TrainSVM
 			values = new double[M];
 			
 			//fill up values
-			values[0] = rs.getInt("contributors"); 
+			/*values[0] = rs.getInt("contributors"); 
 			values[1] = rs.getInt("retweetcount");
 			values[2] = rs.getInt("usermention");
 			values[3] = rs.getInt("hashtag");
@@ -109,23 +113,54 @@ public class TrainSVM
 			values[14] = rs.getDouble("wordscore");
 			values[15] = rs.getDouble("corpusscore");
 			values[16] = rs.getInt("editdistance");
-			values[17] = rs.getInt("4sq");
+			values[17] = rs.getInt("4sq");*/
+			//values[0] = rs.getInt("contributors"); 
+			//values[1] = rs.getInt("retweetcount");
+			//values[2] = rs.getInt("usermention");
+			//values[3] = rs.getInt("hashtag");
+			values[0] = rs.getInt("url");
+			//values[5] = rs.getInt("favorited");
+		//	values[6] = rs.getInt("media");
+			values[1] = rs.getInt("favorites");
+			values[2] = rs.getInt("friends");
+			values[3] = rs.getInt("followers");
+			//values[10] = rs.getInt("listed");
+			values[4] = rs.getInt("statuses");
+		//	values[12] = rs.getInt("verified");
+			values[5] = rs.getInt("gramsmatched");
+			values[6] = rs.getDouble("wordscore");
+			values[7] = rs.getDouble("corpusscore");
+			values[8] = rs.getInt("editdistance");
+			values[9] = rs.getInt("4sq");
 			
 			trainData[i] = new LabeledFeatureVector(label, dimensions, values);
 			trainData[i].normalizeL2();
 			i++;
 		}
 		
+		
 		// Initialize a new TrainingParamteres object with the default SVM-light
 	    // values
 	    TrainingParameters tp = new TrainingParameters();
 
 	    // Switch on some debugging output
+	    tp.getKernelParameters().kernel_type = 1;
 	    tp.getLearningParameters().verbosity = 1;
-
-	    System.out.println("\nTRAINING SVM-light MODEL ..");
-	    SVMLightModel model = trainer.trainModel(trainData, tp);
+	    //tp.getLearningParameters().svm_c_factor = 100;
 	    
+	    
+	    
+	    
+	    System.out.println("\nTRAINING SVM-light MODEL ..");
+	   // SVMLightModel model = trainer.trainModel(trainData, tp);
+	    String[] argv = new String[6];
+	    argv[0] = "-v";
+	    argv[1] = "1";
+	    argv[2] = "-t";
+	    argv[3] = "1";
+	    argv[4] = "-j";
+	    argv[5] = "25";
+	    SVMLightModel model = trainer.trainModel(trainData, argv);
 	    //Write model to file
 	    model.writeModelToFile("socialbuzz.svm.model");
 	    System.out.println(" DONE.");	
@@ -134,6 +169,8 @@ public class TrainSVM
 	 // Use the classifier on the randomly created feature vectors
 	    System.out.println("\nVALIDATING SVM-light MODEL in Java..");
 	    int precision = 0;
+	    
+	    int t=0, tn=0, fp=0, fn=0;
 	    for (int j = 0; j < N; j++) {
 
 	      // Classify a test vector using the Java object
@@ -145,6 +182,17 @@ public class TrainSVM
 	    	  
 	        precision++;
 	      }
+	      
+	      
+	      if (trainData[j].getLabel() > 0 && d > 0)
+	    	  t++;
+	      if (trainData[j].getLabel() < 0 && d < 0)
+	    	  tn++;
+	      if (trainData[j].getLabel() < 0 && d > 0)
+	    	  fp++;
+	      if (trainData[j].getLabel() > 0 && d < 0)
+	    	  fn++;
+	      
 	      if (d>0) System.out.println("positive!");
 	      if (j % 10 == 0) {
 	        System.out.print(i + ".");
@@ -154,5 +202,6 @@ public class TrainSVM
 	    System.out.println(" DONE.");
 	    System.out.println("\n" + ((double) precision / N)
 	        + " PRECISION=RECALL ON RANDOM TRAINING SET.");
+	    System.out.println("TP : "+t+"\tTN : "+tn+"\tFP : "+fp+"\tFN : "+fn);
 	}
 }
